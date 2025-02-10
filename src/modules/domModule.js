753779f4a3cccae480b31b9834e93b076f2fa7a7
huskarnov <143254754+Huskarnov@users.noreply.github.com>
2025-02-10 09:50:17 +0100

    x

--- src/modules/domModule.js
+++ src/modules/domModule.js
@@ -6,6 +6,7 @@ export {renderCards, giveEvents};
 const cardGrid = document.querySelector('.card-grid');
 
 
+//--------------------------------------------------------------------------------
 
 const createCard = function(project){
     let card = document.createElement('div');
@@ -21,7 +22,20 @@ const createCard = function(project){
     description.textContent = project.description;
     dueDate.textContent = project.dueDate;
     crossDelete.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M6 19a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6zm2.46-7.12l1.41-1.41L12 12.59l2.12-2.12l1.41 1.41L13.41 14l2.12 2.12l-1.41 1.41L12 15.41l-2.12 2.12l-1.41-1.41L10.59 14zM15.5 4l-1-1h-5l-1 1H5v2h14V4z"/></svg>';
+    crossDelete.style.display ='none';
     editProject.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M19 19V5H5v14zm0-16a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2zm-2.3 6.35l-1 1l-2.05-2.05l1-1c.21-.22.56-.22.77 0l1.28 1.28c.22.21.22.56 0 .77M7 14.94l6.06-6.06l2.06 2.06L9.06 17H7z"/></svg>';
+    editProject.style.display = 'none';
+
+
+    card.addEventListener('mouseenter', ()=>{
+        crossDelete.style.display = 'block';
+        editProject.style.display = 'block';
+    });
+    card.addEventListener('mouseleave', ()=>{
+        crossDelete.style.display = 'none';
+        editProject.style.display = 'none';
+    });
+
 
     card.appendChild(title);
     card.appendChild(description);
@@ -32,6 +46,7 @@ const createCard = function(project){
     return card;
 };
 
+//--------------------------------------------------------------------------------
 
 //uses create card for every project data to generate a card
 const renderCards = function(){
@@ -41,24 +56,27 @@ const renderCards = function(){
         cardGrid.appendChild(createCard(projects[i]));
     }
 }
+//--------------------------------------------------------------------------------
 
 const clearCards = function(){
     for(let i = 0;i <= cardGrid.children.length - 1; i++){
         cardGrid.removeChild(cardGrid.children[i]);
     };
 };
+//--------------------------------------------------------------------------------
 
-//gives event listeners to elements
+//open new project form
 function giveEvents(){
-    //new project event
     const newProject = document.querySelector('header svg');
     const dialog = document.querySelector('dialog');
 
         newProject.addEventListener('click', ()=>{
         dialog.showModal();
     });
-    
-    //submit/cancel buttons
+
+//--------------------------------------------------------------------------------
+
+    //submit event
     const projectForm = document.querySelector('.new-project');
 
     projectForm.addEventListener('submit', (e)=>{
@@ -68,7 +86,7 @@ function giveEvents(){
         const projectObj = {
             title: data.get('title'),
             description: data.get('description'),
-            dueDate: data.get('dueDate'),
+            dueDate: (data.get('dueDate')).replaceAll('-','/'),
             checkList: [],
         };
 
@@ -82,7 +100,7 @@ function giveEvents(){
 
 
     });
-
+//--------------------------------------------------------------------------------
     const cancelButton = document.querySelector('#new-project-cancel');
     cancelButton.addEventListener('click', (e)=>{
         e.preventDefault();
@@ -91,3 +109,4 @@ function giveEvents(){
     });
 
 };
+//--------------------------------------------------------------------------------
